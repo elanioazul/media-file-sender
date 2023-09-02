b4e85d0ac6d5a712a1e0ef730ce3bce121b41efa
hugo <hugo.herrador@gmail.com>
2023-09-02 18:47:34 +0200

    more test related to paths. Absolute path in windows

--- src/index.ts
+++ src/index.ts
@@ -27,3 +27,21 @@ const otherInfo = {
 console.log(systemUptime);
 console.log(userInfo);
 console.log(otherInfo);
+
+
+// Import 'path' module using the 'require()' method:
+const path = require('path')
+
+// Assigning a path to the myPath variable
+const myPath = 'C:/Users/hugoh/Documents/projects_personal/media-file-sender/index.ts'
+
+const pathInfo = {
+    fileName: path.basename(myPath),
+    folderName: path.dirname(myPath),
+    fileExtension: path.extname(myPath),
+    absoluteOrNot: path.isAbsolute(myPath),
+    detailInfo: path.parse(myPath),
+}
+
+// Let's See The Results:
+console.log(pathInfo);
