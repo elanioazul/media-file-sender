848c6fd070b74a0e2483c55661b5f48fbf7b8705
hugo <hugo.herrador@gmail.com>
2023-09-02 18:32:48 +0200

    get information about the OS and dfaullt env var

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('Hello world')
+console.log('Hello world, I am back!')
+
+// __dirname Global Variable
+console.log(__dirname);
+
+// __filename Global Variable
+console.log(__filename);
+
+
+const os = require('os');
+
+// os.uptime()
+const systemUptime = os.uptime();
+
+// os.userInfo()
+const userInfo = os.userInfo();
+
+// We will store some other information about my WindowsOS in this object:
+const otherInfo = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+
+// Let's Check The Results:
+console.log(systemUptime);
+console.log(userInfo);
+console.log(otherInfo);
