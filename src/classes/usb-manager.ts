87b05122ac26cf2aec1b1bb2728b5e0b5bf6e6b4
Pi 2b hugoherrador <hugo.herrador@gmail.com>
2023-11-19 00:51:42 +0100

    app lists directories of usb

--- src/classes/usb-manager.ts
+++ src/classes/usb-manager.ts
@@ -1,145 +1,76 @@
-import { EventEmitter } from 'events';
-import { usb, getDeviceList, findByIds, findBySerialNumber, WebUSBDevice, webusb, WebUSB} from 'usb';
-import { UsbObserver } from '../interfaces/usb-observer-interface';
+import * as usb from 'usb';
+import * as fs from 'fs';
+import * as path from 'path';
+
 import { env } from 'process';
 import dotenv from 'dotenv';
 dotenv.config();
 
 export class UsbManager {
-  private usbEmitter: EventEmitter = new EventEmitter();
-  private usbDevice: usb.Device | null = null;
 
-  constructor(private vendorId: any, private productId: any) {
-    
-    //this.initUsb();
-    const targetPapaSerialNumber = '12100602000243';
-    const targetSerialNumber = '001D0F1886A15B890B020984';
-    const targetTeeCamSerialNumber = 'ú';
-    // const matchingDevice = this.findDeviceBySerialNumber(targetSerialNumber);
+  constructor(private vendorId: any, private productId: any) {}
 
-    // if (matchingDevice) {
-    //   console.log('Found the matching USB device:', matchingDevice);
-    // } else {
-    //   console.log('No matching USB device found.');
-    // }
-  //   (async () => {
-  //     // Uses a blocking call, so is async
-  //     const device = await findBySerialNumber(targetTeeCamSerialNumber);
+  getStringDescriptor = (device: usb.Device, desc_index: number): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      device.getStringDescriptor(desc_index, (error, value) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(value || '');
+        }
+      });
+    });
+  };
+
+  findDevice = (): usb.Device | undefined => {
+    const devices = usb.getDeviceList();
+    return devices.find(device => device.deviceDescriptor.idVendor === this.vendorId && device.deviceDescriptor.idProduct === this.productId);
+  };
+
+  logDeviceDetails = async (device: usb.Device): Promise<void> => {
+    device.open();
   
-  //     if (device) {
-  //         console.log(device); // Legacy device
-  //     } else {
-  //       console.log('no device found from usb findBySerialNumber function')
-  //     }
-  // })();
-//   (async () => {
-//     // Uses a blocking call, so is async
-//     const device = await findBySerialNumber(targetTeeCamSerialNumber);
+    const deviceDescriptor: any = device.deviceDescriptor;
+  
+    console.log('USB Mass Storage Device Details:');
+    console.log('Vendor ID:', deviceDescriptor.idVendor);
+    console.log('Product ID:', deviceDescriptor.idProduct);
+  
+    const manufacturer = await this.getStringDescriptor(device, deviceDescriptor.iManufacturer);
+    const product = await this.getStringDescriptor(device, deviceDescriptor.iProduct);
+    const serialNumber = await this.getStringDescriptor(device, deviceDescriptor.iSerial);
+  
+    console.log('Manufacturer:', manufacturer);
+    console.log('Product:', product);
+    console.log('Serial Number:', serialNumber);
+  
+    device.close();
+  };
 
-//     // Uses blocking calls, so is async
-//     if (device) {
-//       const webDevice = await WebUSBDevice.createInstance(device);
-//       if (webDevice) {
-//           console.log(webDevice); // WebUSB device
-//       }
-//     }
-
-// })();
-// (async () => {
-//   // Returns first matching device
-//   const device = await webusb.requestDevice({
-//       filters: [{}]
-//   })
-
-//   console.log(device); // WebUSB device
-// })();
-// (async () => {
-//   const customWebUSB = new WebUSB({
-//       // This function can return a promise which allows a UI to be displayed if required
-//       devicesFound: (devices: any) => devices.find((device: any) => device.serialNumber === targetPapaSerialNumber)
-//   });
-
-//   // Returns device based on injected 'devicesFound' function
-//   const device = await customWebUSB.requestDevice({
-//       filters: [{}]
-//   })
-
-//   console.log(device); // WebUSB device
-// })();
-    const device = findByIds(vendorId, productId);
-
-    if (device) {
-        console.log(device); // Legacy device
+  listDirectories = async (directory: string): Promise<void> => {
+    try {
+      const files = await fs.promises.readdir(directory);
+  
+      // Filter only directories
+      const directories = await Promise.all(
+        files.map(async file => {
+          const filePath = path.join(directory, file);
+          const fileStats = await fs.promises.stat(filePath);
+  
+          if (fileStats.isDirectory()) {
+            return file;
+          }
+          return null;
+        })
+      );
+  
+      // Filter out null values (non-directories)
+      const actualDirectories = directories.filter(dir => dir !== null);
+  
+      console.log(`Directories in ${directory}:`);
+      console.log(actualDirectories);
+    } catch (error) {
+      console.error(`Error listing directories: ${error}`);
     }
-    // const devices = getDeviceList();
-
-    // for (const device of devices) {
-    //     console.log(device); // Legacy device
-    // }
-  }
-
-  // findDeviceBySerialNumber(serialNumber: string) {
-  //   // Get a list of all USB devices
-  //   const deviceList = usb.getDeviceList();
-  
-  //   // Iterate through the list of devices
-  //   for (const device of deviceList) {
-  //     try {
-  //       // Open the device to access its properties
-  //       device.open();
-  
-  //       // Get the serial number of the device
-  //       //const deviceSerialNumber = device.getStringDescriptor(2); // Assumes serial number is at index 2
-  
-  //       // Close the device after accessing its properties
-  //       device.close();
-  
-  //       // Check if the serial number matches the target
-  //       // if (deviceSerialNumber === serialNumber) {
-  //       //   return device; // Found a matching device
-  //       // }
-  //     } catch (error) {
-  //       console.error('Error accessing device:', error);
-  //     }
-  //   }
-  
-  //   return null; // No matching device found
-  // }
-
-  // private initUsb() {
-  //   usb.on('attach', (device) => {
-  //     console.log('usb on???')
-  //     if (this.isTargetDevice(device)) {
-  //       this.usbDevice = device;
-  //       this.usbEmitter.emit('attach', device);
-  //     }
-  //   });
-
-  //   usb.on('detach', (device) => {
-  //     if (this.isTargetDevice(device)) {
-  //       this.usbDevice = null;
-  //       this.usbEmitter.emit('detach', device);
-  //     }
-  //   });
-  // }
-
-  // private isTargetDevice(device: usb.Device): boolean {
-  //   //return process.env.USBSERIALNUMBER === usb.
-  //   // return (
-  //   //   device.deviceDescriptor.idVendor === this.vendorId &&
-  //   //   device.deviceDescriptor.idProduct === this.productId
-  //   // );
-  // }
-
-  public attachObserver(observer: UsbObserver) {
-    console.log('attaching??')
-    this.usbEmitter.on('attach', (device) => observer.onUsbAttach(device));
-  }
-
-  public detachObserver(observer: UsbObserver) {
-    console.log('detaching???')
-    this.usbEmitter.on('detach', (device) => observer.onUsbDetach(device));
-  }
+  };
 }
-//export { UsbObserver };
-
--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,16 @@
-import * as usb from 'usb';
+import { UsbManager } from './classes/usb-manager';
 
+//my usb device details
 const VENDOR_ID = 0x0930;
 const PRODUCT_ID = 0x6545;
+const sourceDirectory = '/media/hugoherrador/MI PEN';
 
-interface USBDeviceDescriptor {
-  idVendor: number;
-  idProduct: number;
-  iManufacturer: number;
-  iProduct: number;
-  iSerial: number;
-}
+const usbManager = new UsbManager(VENDOR_ID, PRODUCT_ID); 
+const found = usbManager.findDevice();
 
-const findDevice = (): usb.Device | undefined => {
-  const devices = usb.getDeviceList();
-  return devices.find(device => device.deviceDescriptor.idVendor === VENDOR_ID && device.deviceDescriptor.idProduct === PRODUCT_ID);
-};
-
-const logDeviceDetails = async (device: usb.Device): Promise<void> => {
-  device.open();
-
-  const deviceDescriptor: any = device.deviceDescriptor;
-
-  console.log('USB Mass Storage Device Details:');
-  console.log('Vendor ID:', deviceDescriptor.idVendor);
-  console.log('Product ID:', deviceDescriptor.idProduct);
-
-  const manufacturer = await getStringDescriptor(device, deviceDescriptor.iManufacturer);
-  const product = await getStringDescriptor(device, deviceDescriptor.iProduct);
-  const serialNumber = await getStringDescriptor(device, deviceDescriptor.iSerial);
-
-  console.log('Manufacturer:', manufacturer);
-  console.log('Product:', product);
-  console.log('Serial Number:', serialNumber);
-
-  device.close();
-};
-
-const getStringDescriptor = (device: usb.Device, desc_index: number): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    device.getStringDescriptor(desc_index, (error, value) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(value || '');
-      }
-    });
-  });
-};
-
-const usbDevice = findDevice();
-
-if (usbDevice) {
-  logDeviceDetails(usbDevice).catch(error => console.error('Error:', error));
+if (found) {
+  usbManager.logDeviceDetails(found).catch(error => console.error('Error:', error));
+  usbManager.listDirectories(sourceDirectory);
 } else {
   console.log('USB mass storage device not found.');
 }
