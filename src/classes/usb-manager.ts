c46548cd51ad7a1be7c8eaca41ad951b240d1740
Pi 2b hugoherrador <hugo.herrador@gmail.com>
2023-11-19 20:04:06 +0100

    not really wathcing yet but good order

--- src/classes/usb-manager.ts
+++ src/classes/usb-manager.ts
@@ -1,6 +1,7 @@
 import * as usb from 'usb';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as chokidar from 'chokidar';
 
 import { env } from 'process';
 import dotenv from 'dotenv';
@@ -44,7 +45,7 @@ export class UsbManager {
     console.log('Product:', product);
     console.log('Serial Number:', serialNumber);
   
-    device.close();
+    //device.close();
   };
 
   listDirectories = async (directory: string): Promise<void> => {
@@ -92,5 +93,36 @@ export class UsbManager {
       console.error(`Error listing directories: ${error}`);
       return null;
     }
+  };
+
+  watchDirectory = (directory: string): void => {
+    console.log(`Watching directory: ${directory}`);
+  
+    const watcher = chokidar.watch(directory, {
+      ignored: /^\./, // ignore dot files
+      persistent: true,
+      ignoreInitial: true, // do not trigger for existing files
+    });
+  
+    watcher.on('add', async filePath => {
+      const fileExtension = path.extname(filePath).toLowerCase();
+  
+      if (['.jpg', '.png', '.avi', '.mp4'].includes(fileExtension)) {
+        try {
+          const fileStats = await fs.promises.stat(filePath);
+  
+          console.log(`New file detected: ${path.basename(filePath)}`);
+          console.log(`File path: ${filePath}`);
+          console.log(`File size: ${fileStats.size} bytes`);
+          console.log(`Last modified: ${fileStats.mtime}`);
+        } catch (error) {
+          console.error(`Error reading file details: ${error}`);
+        }
+      }
+    });
+  
+    watcher.on('error', error => {
+      console.error(`Watcher error: ${error}`);
+    });
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,30 @@ const sourceDirectory = '/media/hugoherrador/MI PEN';
 const targetDirectory = 'camfiles-source';
 
 const usbManager = new UsbManager(VENDOR_ID, PRODUCT_ID); 
-const found = usbManager.findDevice();
+
 
 const main = async () => {
   try {
-    const foundDirectory = await usbManager.findDirectory(sourceDirectory, targetDirectory);
+    const foundTargetDirectory = await usbManager.findDirectory(sourceDirectory, targetDirectory);
+    const device = usbManager.findDevice();
 
-    if (foundDirectory) {
-      console.log(`Found target directory: ${foundDirectory}`);
+    if (foundTargetDirectory && device !== undefined) {
+      await usbManager.logDeviceDetails(device).catch(error => console.error('Error:', error));
+      await usbManager.listDirectories(sourceDirectory);
+      console.log(`Found target directory: ${foundTargetDirectory}`);
+      usbManager.watchDirectory(targetDirectory); 
     } else {
       console.log(`Target directory not found.`);
     }
   } catch (error) {
-    console.error(`Error finding target directory: ${error}`);
+    console.error(`Error finding target directory in device: ${error}`);
   }
 };
-
-if (found) {
-  usbManager.logDeviceDetails(found).catch(error => console.error('Error:', error));
-  usbManager.listDirectories(sourceDirectory);
-  main();
-} else {
-  console.log('USB mass storage device not found.');
-}
+main();
+// if (found) {
+//   usbManager.logDeviceDetails(found).catch(error => console.error('Error:', error));
+//   usbManager.listDirectories(sourceDirectory);
+//   main();
+// } else {
+//   console.log('USB mass storage device not found.');
+// }
